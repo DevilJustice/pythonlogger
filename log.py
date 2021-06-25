8cee1f77f0ce159d4b3cf2c5aabdfe58bbb0aaef
DevilJustice <86474477+DevilJustice@users.noreply.github.com>
2021-06-25 06:56:51 -0500

    Create log.py

---
+++ log.py
@@ -0,0 +1,12 @@
+import brain
+from brain import deviljustice
+
+def Log():
+    try:
+        log.user()
+   except:
+          pass
+          
+Log()
+print(f'logged by deviljustice:rofl:""")
+// <--- its javascript btw nd i win always!!!
